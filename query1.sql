0c1e41b2d4a56337f2c3dcef43453a4d8129c167
IgnacioSerena <124685864+IgnacioSerena@users.noreply.github.com>
2023-10-24 11:24:29 +0200

    Add files via upload

--- query1.sql
+++ query1.sql
@@ -2,7 +2,6 @@
 WITH ReservasIdaVuelta AS (
   SELECT
     b.book_ref,
-    t.ticket_no,
     f1.departure_airport AS aeropuerto_salida,
     f2.arrival_airport AS aeropuerto_llegada
   FROM
@@ -13,14 +12,13 @@ WITH ReservasIdaVuelta AS (
     JOIN ticket_flights tf2 ON t.ticket_no = tf2.ticket_no
     JOIN flights f2 ON tf2.flight_id = f2.flight_id
   WHERE
-    f1.scheduled_departure = (SELECT MIN(scheduled_departure) FROM flights WHERE flight_id = tf1.flight_id)
-    AND f2.scheduled_arrival = (SELECT MAX(scheduled_arrival) FROM flights WHERE flight_id = tf2.flight_id)
+    f1.scheduled_departure < f2.scheduled_departure
     AND f1.departure_airport = f2.arrival_airport
 )
 SELECT
-  riv.aeropuerto_salida AS codigo_aeropuerto,
+  rv.aeropuerto_salida AS codigo_aeropuerto,
   COUNT(DISTINCT rv.book_ref) AS num_reservas
 FROM
-  ReservasIdaVuelta riv
+  ReservasIdaVuelta rv
 GROUP BY
-  riv.aeropuerto_salida;
+  rv.aeropuerto_salida;
