5c0d747d95de4d03219cccfdd00919332bb53495
IgnacioSerena <124685864+IgnacioSerena@users.noreply.github.com>
2023-10-26 18:53:32 +0200

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -8,11 +8,11 @@ WITH ReservasIdaVuelta AS (
     bookings b
     JOIN tickets t ON b.book_ref = t.book_ref
     JOIN ticket_flights tf1 ON t.ticket_no = tf1.ticket_no
-    JOIN flights f1 ON tf1.flight_id = f1.flight_id
+    JOIN flights f1 ON tf1.flight_id = f1.flight_id 
     JOIN ticket_flights tf2 ON t.ticket_no = tf2.ticket_no
     JOIN flights f2 ON tf2.flight_id = f2.flight_id
   WHERE
-    f1.scheduled_departure < f2.scheduled_departure
+    f1.scheduled_arrival < f2.scheduled_departure
     AND f1.departure_airport = f2.arrival_airport
 )
 SELECT
