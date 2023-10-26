bc81def040cdd41f7c89d4cb9902a5622f0a7450
IgnacioSerena <124685864+IgnacioSerena@users.noreply.github.com>
2023-10-26 18:48:30 +0200

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,9 +1,6 @@
 --Consulta 2
-SELECT
-    b.book_ref AS "booking.book_ref",
-    b.total_amount AS "booking.total_amount",
-    SUM(tf.amount) AS "Precio Calculado"
-FROM bookings AS b
-JOIN ticket_flights AS tf ON b.book_ref = tf.ticket_no
+SELECT b.book_ref, b.total_amount, SUM(tf.amount) AS calculated_amount
+FROM bookings b,tickets t, ticket_flights tf
+WHERE b.book_ref = t.book_ref AND t.ticket_no = tf.ticket_no
 GROUP BY b.book_ref, b.total_amount
-ORDER BY b.book_ref;
+ORDER BY b.book_ref ASC;
