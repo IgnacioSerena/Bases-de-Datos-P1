aa243fbd352a772b706f0d0d103a0e968dbd4317
IgnacioSerena <124685864+IgnacioSerena@users.noreply.github.com>
2023-10-26 18:46:47 +0200

    Update query4.sql

--- query4.sql
+++ query4.sql
@@ -1,13 +1,25 @@
 --Consulta 4
-WITH VacantSeats AS (
+WITH EmptySeats AS (
     SELECT
-        flight_id,
-        COUNT(*) AS "Asientos Vacíos"
-    FROM seats
-    GROUP BY flight_id
+        f.flight_id,
+        f.aircraft_code,
+        COUNT(s.seat_no) AS num_empty_seats
+    FROM
+        flights AS f
+    LEFT JOIN
+        seats AS s ON f.aircraft_code = s.aircraft_code
+    LEFT JOIN
+        ticket_flights AS tf ON f.flight_id = tf.flight_id
+    WHERE
+        tf.ticket_no IS NULL
+    GROUP BY
+        f.flight_id, f.aircraft_code
 )
+
 SELECT
-    vs.flight_id AS "flight id",
-    vs."Asientos Vacíos" AS "Número de Asientos Vacíos"
-FROM VacantSeats AS vs
-WHERE vs."Asientos Vacíos" = (SELECT MAX("Asientos Vacíos") FROM VacantSeats);
+    es.flight_id,
+    es.num_empty_seats
+FROM
+    EmptySeats AS es
+WHERE
+    es.num_empty_seats = (SELECT MAX(num_empty_seats) FROM EmptySeats);
