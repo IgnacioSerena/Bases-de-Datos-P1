41b0ec55ab034a07e3db9a8b9e3c3f5defe493cf
IgnacioSerena <124685864+IgnacioSerena@users.noreply.github.com>
2023-10-27 03:41:14 +0200

    Update query3.sql

--- query3.sql
+++ query3.sql
@@ -1,8 +1,8 @@
 --Consulta 3
-SELECT
-    f.arrival_airport AS "Código de Aeropuerto",
-    COUNT(bp.ticket_no) AS "Número de Pasajeros Recibidos"
-FROM flights AS f
-LEFT JOIN boarding_passes AS bp ON f.flight_id = bp.flight_id
-GROUP BY f.arrival_airport
-ORDER BY "Número de Pasajeros Recibidos" ASC;
+SELECT f.arrival_airport   AS "Código de Aeropuerto",
+       Count(bp.ticket_no) AS "Número de Pasajeros Recibidos"
+FROM   flights AS f
+       LEFT JOIN boarding_passes AS bp
+              ON f.flight_id = bp.flight_id
+GROUP  BY f.arrival_airport
+ORDER  BY "Número de Pasajeros Recibidos" ASC;
