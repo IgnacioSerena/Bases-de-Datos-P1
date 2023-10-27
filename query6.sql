e9bbcb7952668acd8aabf2bec493ecd4b636b025
IgnacioSerena <124685864+IgnacioSerena@users.noreply.github.com>
2023-10-27 03:44:31 +0200

    Update query6.sql

--- query6.sql
+++ query6.sql
@@ -1,27 +1,13 @@
 --Consulta 6
-WITH Retrasos AS (
-  SELECT
-    f.flight_no,
-    (f.actual_arrival - f.scheduled_arrival) AS retraso
-  FROM
-    flights f
-)
-SELECT
-  r.flight_no,
-  AVG(r.retraso) AS retraso_promedio
-FROM
-  Retrasos r
-GROUP BY
-  r.flight_no
-HAVING
-  AVG(r.retraso) = (
-    SELECT
-      MAX(avg_retraso)
-    FROM
-      (SELECT
-        AVG(retraso) AS avg_retraso
-      FROM
-        Retrasos
-      GROUP BY
-        flight_no) AS subquery
-  );
+WITH retrasos
+     AS (SELECT f.flight_no,
+                ( f.actual_arrival - f.scheduled_arrival ) AS retraso
+         FROM   flights f)
+SELECT r.flight_no,
+       Avg(r.retraso) AS retraso_promedio
+FROM   retrasos r
+GROUP  BY r.flight_no
+HAVING Avg(r.retraso) = (SELECT Max(avg_retraso)
+                         FROM   (SELECT Avg(retraso) AS avg_retraso
+                                 FROM   retrasos
+                                 GROUP  BY flight_no) AS subquery);
