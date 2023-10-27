4590cceb97844ccfffca5c9899cd9e273fc7d330
IgnacioSerena <124685864+IgnacioSerena@users.noreply.github.com>
2023-10-27 03:43:49 +0200

    Update query5.sql

--- query5.sql
+++ query5.sql
@@ -1,8 +1,13 @@
 --Consulta 5
-SELECT b.book_ref, tf.flight_id
-FROM bookings b 
-LEFT JOIN tickets t ON b.book_ref = t.book_ref
-LEFT JOIN ticket_flights tf ON t.ticket_no = tf.ticket_no
-LEFT JOIN boarding_passes bp ON tf.ticket_no = bp.ticket_no
-WHERE bp.boarding_no is NULL
-ORDER BY b.book_ref ASC, bp.flight_id ASC;
+SELECT b.book_ref,
+       tf.flight_id
+FROM   bookings b
+       LEFT JOIN tickets t
+              ON b.book_ref = t.book_ref
+       LEFT JOIN ticket_flights tf
+              ON t.ticket_no = tf.ticket_no
+       LEFT JOIN boarding_passes bp
+              ON tf.ticket_no = bp.ticket_no
+WHERE  bp.boarding_no IS NULL
+ORDER  BY b.book_ref ASC,
+          bp.flight_id ASC;
